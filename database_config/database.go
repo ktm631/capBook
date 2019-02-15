1b1b7d8e5ee8073d1d6c646547461031697c4c3a
ktm_631 <ktm631@gmail.com>
2019-02-15 13:17:38 +0100

    Orm connection done

---
+++ database_config/database.go
@@ -0,0 +1,7 @@
+package database_config
+
+import "github.com/jinzhu/gorm"
+
+var (
+	DBCon *gorm.DB
+)
--- server/server.go
+++ server/server.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"capBook"
+	"capBook/database_config"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/99designs/gqlgen/handler"
 )
 
 const defaultPort = "8080"
+
+var db *gorm.DB
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-
+	db = database_config.DbConn()
+	defer db.Close()
+	db.AutoMigrate(&capBook.User{})
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
 	http.Handle("/query", handler.GraphQL(capBook.NewExecutableSchema(capBook.Config{Resolvers: &capBook.Resolver{}})))
 
