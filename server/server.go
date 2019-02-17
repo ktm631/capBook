58497d81e1a08ccb3673dde5edd5b5aa9c161f15
ktm_631 <ktm631@gmail.com>
2019-02-17 14:50:55 +0100

    CORS

--- server/server.go
+++ server/server.go
@@ -24,8 +24,22 @@ func main() {
 	defer db.Close()
 	db.AutoMigrate(&capBook.User{}, &capBook.Author{}, &capBook.Book{}, &capBook.Location{}, &capBook.Publisher{}, &capBook.Rental{})
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(capBook.NewExecutableSchema(capBook.Config{Resolvers: &capBook.Resolver{}})))
+	http.Handle("/query", corsAccess(handler.GraphQL(capBook.NewExecutableSchema(capBook.Config{Resolvers: &capBook.Resolver{}}))))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func corsAccess(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if req.Method == "OPTIONS" {
+			return
+		}
+		next(w, req)
+	})
 }
